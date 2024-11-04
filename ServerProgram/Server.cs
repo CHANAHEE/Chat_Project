f91e668d404bb651993ec7c15812e76b48330aba
CHANAHEE <113041686+CHANAHEE@users.noreply.github.com>
2024-11-04 23:03:48 +0900

    Receive And Send Server

--- ServerProgram/Server.cs
+++ ServerProgram/Server.cs
@@ -52,11 +52,16 @@ namespace ServerProgram
 
         public void Start(IPEndPoint localEndPoint)
         {
+            // 소켓 생성
             serverSocket = new Socket(localEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             
+            // 소켓 바인딩 (소켓이 수신할 IP 주소 및 포트 설정)
             serverSocket.Bind(localEndPoint);
+
+            // 소켓 리스닝 (소켓 수신대기 및 갯수 설정)
             serverSocket.Listen(100);
 
+            // 비동기 작업에 대한 정보 설정
             SocketAsyncEventArgs acceptEventArg = new SocketAsyncEventArgs();
             acceptEventArg.Completed += new EventHandler<SocketAsyncEventArgs>(AcceptEventArg_Completed);
             
@@ -70,9 +75,13 @@ namespace ServerProgram
                 bool willRaiseEvent = false;
                 while (!willRaiseEvent)
                 {
+                    // 클라이언트 최대 수 제한
                     maxNumberAcceptedClients.WaitOne();
-
+                    
+                    // 이전 수락 작업 초기화
                     acceptEventArg.AcceptSocket = null;
+
+                    // 비동기 수락 작업 : 즉시 반환이면 true, 아니면 false
                     willRaiseEvent = serverSocket.AcceptAsync(acceptEventArg);
                     if (!willRaiseEvent)
                     {
@@ -97,20 +106,28 @@ namespace ServerProgram
         {
             mainForm.Update_Message(DateTime.Now.ToString(), "Client Connected!");
 
+            // Pool 에서 SocketAsyncEventArgs 객체 가져오기
             SocketAsyncEventArgs readEventArgs = readWritePool.Pop();
-            readEventArgs.UserToken = e.AcceptSocket;
-            readEventArgs.Completed += ReceiveEventArg_Completed;
 
+            // 클라이언트가 연결된 소켓을 할당하여 추후 작업 시 해당 소켓 참조 
+            readEventArgs.UserToken = e.AcceptSocket;
+
+            // 비동기 수신 시작
             bool willRaiseEvent = e.AcceptSocket.ReceiveAsync(readEventArgs);
             if (!willRaiseEvent)
             {
-                ProcessReceive(readEventArgs);
+                switch (e.LastOperation)
+                {
+                    case SocketAsyncOperation.Receive:
+                        ProcessReceive(e);
+                        break;
+                    case SocketAsyncOperation.Send:
+                        ProcessSend(e);
+                        break;
+                    default:
+                        throw new ArgumentException("The last operation completed on the socket was not a receive or send");
+                }
             }
-        }
-
-        void ReceiveEventArg_Completed(object sender, SocketAsyncEventArgs e)
-        {
-            ProcessReceive(e);
         }
 
         void IO_Completed(object sender, SocketAsyncEventArgs e)
@@ -135,14 +152,16 @@ namespace ServerProgram
                 string message = Encoding.UTF8.GetString(e.Buffer, 0, e.BytesTransferred);
                 mainForm.Update_Message(DateTime.Now.ToString(), message);
 
-                e.SetBuffer(e.Offset, e.BytesTransferred);
-                Socket socket = (Socket)e.UserToken;
-                bool willRaiseEvent = socket.SendAsync(e);
+                //e.SetBuffer(e.Offset, e.BytesTransferred);
+                //Socket socket = (Socket)e.UserToken;
+                //bool willRaiseEvent = socket.SendAsync(e);
 
-                if (!willRaiseEvent)
-                {
-                    ProcessSend(e);
-                }
+                //if (!willRaiseEvent)
+                //{
+                //    ProcessSend(e);
+                //}
+
+                ProcessSend(e);
             }
             else
             {
--- ServerProgram/obj/x64/Debug/net8.0-windows/ServerProgram.AssemblyInfo.cs
+++ ServerProgram/obj/x64/Debug/net8.0-windows/ServerProgram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ee9446615c29ccdc01cdc85cafc7d10615b3e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9cebd09999f7a74599aa06f9feb6a88120a8006")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
