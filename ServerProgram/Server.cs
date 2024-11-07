3b9fa0be5d47901e15cade22306d83c42d83b3c4
cha <tjdrjs0803@naver.com>
2024-11-07 09:15:58 +0900

    Server Data Receive

--- ServerProgram/Server.cs
+++ ServerProgram/Server.cs
@@ -98,6 +98,7 @@ namespace ServerProgram
 
         private void ProcessAccept(SocketAsyncEventArgs e)
         {
+            // 서버 UI 에 클라이언트 연결 성공 메시지 추가
             mainForm.Update_Message(DateTime.Now.ToString(), "Client Connected!");
 
             // Pool 에서 SocketAsyncEventArgs 객체 가져오기
@@ -105,29 +106,16 @@ namespace ServerProgram
 
             // 클라이언트가 연결된 소켓을 할당하여 추후 작업 시 해당 소켓 참조 
             readEventArgs.UserToken = e.AcceptSocket;
-            readEventArgs.SetBuffer(new byte[1024],0,1024);
+
+            // readEventArgs 객체의 버퍼를 설정. 1024 바이트만큼 미리 할당. 이후 ReceiveAsync 을 통해 수신된 데이터를 저장할 버퍼.
+            readEventArgs.SetBuffer(new byte[1024], 0, 1024);
+
             // 비동기 수신 시작
             bool willRaiseEvent = e.AcceptSocket.ReceiveAsync(readEventArgs);
             if (!willRaiseEvent)
             {
                 ProcessReceive(e);
             }
-            
-            //if (!willRaiseEvent)
-            //{
-            //    switch (e.LastOperation)
-            //    {
-            //        case SocketAsyncOperation.Receive:
-            //            ProcessReceive(e);
-            //            break;
-            //        case SocketAsyncOperation.Send:
-            //            mainForm.Update_Message(DateTime.Now.ToString(), "SEND");
-            //            //ProcessSend(e);
-            //            break;
-            //        default:
-            //            throw new ArgumentException("The last operation completed on the socket was not a receive or send");
-            //    }
-            //}
         }
 
         void IO_Completed(object sender, SocketAsyncEventArgs e)
@@ -147,14 +135,20 @@ namespace ServerProgram
 
         private void ProcessReceive(SocketAsyncEventArgs e)
         {
+            // 버퍼에 들어있는 데이터의 크기가 있는지와 Socket 의 연결여부를 체크
             if (e.BytesTransferred > 0 && e.SocketError == SocketError.Success)
             {
+                // 버퍼에 들어있는 아스키코드 메시지를 UTF8 로 인코딩하여 string 으로 변환
                 string message = Encoding.UTF8.GetString(e.Buffer, 0, e.BytesTransferred);
+
+                // 서버 UI 에 수신받은 클라이언트 메시지 추가
                 mainForm.Update_Message(DateTime.Now.ToString(), message);
 
+                // 데이터 송신을 위한 버퍼 작업
                 e.SetBuffer(e.Buffer, 0, e.BytesTransferred);
                 Socket socket = (Socket)e.UserToken;
 
+                // 비동기 데이터 송신 작업
                 bool willRaiseEvent = socket.SendAsync(e);
                 if (!willRaiseEvent)
                 {
@@ -173,6 +167,8 @@ namespace ServerProgram
             if (e.SocketError == SocketError.Success)
             {
                 Socket socket = (Socket)e.UserToken;
+                e.SetBuffer(e.Buffer, 0, 1024);
+
                 bool willRaiseEvent = socket.ReceiveAsync(e);
 
                 if (!willRaiseEvent)
